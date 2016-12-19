aad77f8dfa03ea5df2e70748cae76a1e7d7986eb
kiss <kiss72k@daum.net>
2016-12-19 16:32:51 +0900

    second update

---
+++ js/link-page.js
@@ -0,0 +1,18 @@
+/*!
+ * link-page.js
+ * Copyright 2011-2014 My Company, Inc.
+ * Simple javascript file.
+ */
+
+$(".readMore").click(function() {
+    var This=$(this);
+
+    $(this).next().toggle(function() {
+        if(This.text()=="Read") {
+            This.text("Hide") 
+        }
+        else {
+            This.text("Read") 
+        }
+    })
+});
